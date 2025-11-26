b4a0ac6c4009a7849001dfd42b9ff3ec0e3af583
raduolatch <raduola01@gmail.com>
2025-11-26 09:13:24 +0700

    Add files via upload

--- main1.pas
+++ main1.pas
@@ -1,12 +1,75 @@
+uses crt;
+
 var
-    nama,alamat: string;
-    umur: integer;
+  A, B, C: array [1..2, 1..2] of Smallint;
+  i, j, k, determinan: integer;
 
 begin
-    nama:= 'raduola';
-    write('nama saya adalah ');
-    writeln(nama);
-    alamat:= 'jl tutu 1';
-    write(' alamat saya di ');
-    write(alamat);    
+  clrscr;
+
+  // Input matriks A
+  writeln('Input elemen matriks A:');
+  for i := 1 to 2 do
+    for j := 1 to 2 do
+    begin
+      write('A[', i, ',', j, '] = ');
+      readln(A[i, j]);
+    end;
+
+  writeln;
+  writeln('Isi matriks A:');
+  for i := 1 to 2 do
+  begin
+    for j := 1 to 2 do
+      write(A[i, j]:4);
+    writeln;
+  end;
+
+  writeln;
+
+  // Input matriks B
+  writeln('Input elemen matriks B:');
+  for i := 1 to 2 do
+    for j := 1 to 2 do
+    begin
+      write('B[', i, ',', j, '] = ');
+      readln(B[i, j]);
+    end;
+
+  writeln;
+  writeln('Isi matriks B:');
+  for i := 1 to 2 do
+  begin
+    for j := 1 to 2 do
+      write(B[i, j]:4);
+    writeln;
+  end;
+
+  writeln;
+
+  // Proses perkalian matriks C = A * B
+  for i := 1 to 2 do
+    for j := 1 to 2 do
+    begin
+      C[i, j] := 0;
+      for k := 1 to 2 do
+        C[i, j] := C[i, j] + (A[i, k] * B[k, j]);
+    end;
+
+  writeln('Hasil perkalian matriks C = A x B:');
+  for i := 1 to 2 do
+  begin
+    for j := 1 to 2 do
+      write(C[i, j]:6);
+    writeln;
+  end;
+
+  writeln;
+
+  // Hitung determinan matriks C
+  determinan := (C[1,1] * C[2,2]) - (C[1,2] * C[2,1]);
+
+  writeln('Determinan dari matriks C adalah: ', determinan);
+
+  
 end.
--- main2.pas
+++ main2.pas
@@ -1,13 +1,52 @@
+uses crt;
 var
-    nama: string;
-    umur: integer;
-    alamat: string;
+    nama : array[1..5]of string;
+    i,j: integer;
+    matriks: array[1..2 , 1..3]of integer;
+    kata , kata1, kata2 : string;
 begin
-    write('nama anda siapa? ');
-    readln(nama);
-    write('umur anda berapa? ');
-    readln(umur);
-    write('alamat anda di mana? ');
-    readln(alamat);
-    write('halo salam kenal ', nama ,', umur anda ', umur ,', alamat anda di ', alamat);
+    clrscr;
+    //array satu dimensi
+    //  nama[1] := 'endric';
+    //  nama[2] := 'muzayyan';
+    //  nama[3] := 'raduola';
+    //  nama[4] := 'rizky';
+    //  nama[5] := 'dimas';
+
+    //  for i:= 1 to 5 do
+    //     writeln('nama ke -', i , ':', nama[i]);
+
+    //array dua dimensi
+    //  matriks[1,1] := 2;
+    //  matriks[1,2] := 6;
+    //  matriks[1,3] := 8;
+    //  matriks[2,1] := 9;
+    //  matriks[2,2] := 5;
+    //  matriks[2,3] := 4;
+     
+    //  for i:= 1 to 2 do
+    //     begin
+    //         for j := 1 to 3 do
+    //         begin
+    //             write(matriks[i,j]);
+    //         end;
+    //     end;
+
+    // kata1:= 'hello';
+    // kata2:= 'world';
+    // kata:= kata1 + kata2;
+    // writeln('gabungan kata string menjadi:', kata);
+
+
+    //acces string by array
+
+    kata := 'laptop';
+    writeln(kata);
+
+    writeln('karakter pertama :', kata[1]);
+    writeln('karakter pertama :', kata[5]);
+    writeln('banyak karakter : ');
+    
+    for i := 1 to length(kata) do   
+        writeln('huruf ke', i ,': ', kata[i]);
 end.
